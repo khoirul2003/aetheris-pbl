1ecbbb67546e01bd5164df56d9e8a0e1295b2689
khoirul2003 <kanwarudin914@gmail.com>
2026-05-25 18:23:38 +0700

    fix: memperbaiki error dan konflik yg di buat oleh baihaqi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning // Tambahkan baris ini
+    >
       <body className="h-full">
         {children}
       </body>
